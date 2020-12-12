d6b9ddf00fb239c225f4853aadff68a598c0603c
abhishek13yadv <36642139+abhishek13yadv@users.noreply.github.com>
2020-12-12 11:18:46 +0530

    Create Day 1 - Arthmatic Operators.js

---
+++ 10 Days of JavaScript/Day 1 - Arthmatic Operators.js
@@ -0,0 +1,16 @@
+// Area
+function getArea(length, width) {
+    let area;
+    // Write your code here
+    area = length*width;
+    return area;
+}
+
+// Perimeter
+
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    perimeter = 2*(length+width);
+    return perimeter;
+}
